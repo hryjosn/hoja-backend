8d14f29bd57b76f58b82fd94c9f90d9e605e76e3
Johnson <jo74705@gmail.com>
2020-06-26 00:00:47 +0800

    test

--- src/utils/uploadPhoto.js
+++ src/utils/uploadPhoto.js
@@ -4,11 +4,11 @@ import fs from "fs";
 imgur.setClientId("29855e39f65893f");
 imgur.setAPIUrl("https://api.imgur.com/3/");
 // 搜尋相關的 api
-const uploadPhoto = ({ imageUri, album = "", title, description }) => {
-    const imgRes = imgur.uploadFile(imageUri, album, title, description);
-    fs.unlink(imageUri, error => {
-        console.error(error)
-    });
-    return imgRes.data.link;
+const uploadPhoto = async ({ imageUri, album = "", title, description }) => {
+  const imgRes = await imgur.uploadFile(imageUri, album, title, description);
+  fs.unlink(imageUri, (error) => {
+    console.error(error);
+  });
+  return imgRes.data.link;
 };
 export default uploadPhoto;
